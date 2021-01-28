cee024160751b2f79a7014486fdaebd2c170590e
Kevin Burk <percburk@gmail.com>
2021-01-28 16:36:31 -0600

    made favorites button on searchForm

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -3,14 +3,12 @@ import { Box, Typography, Container } from '@material-ui/core';
 import SearchForm from '../SearchForm/SearchForm';
 import SearchList from '../SearchList/SearchList';
 import FavoritesList from '../FavoritesList/FavoritesList';
-import { HashRouter as Router, Route, Link } from 'react-router-dom';
+import { HashRouter as Router, Route } from 'react-router-dom';
 
 function App() {
   return (
     <Router>
-      <Link to="/search">Search</Link>
-      <Link to="/favorites">Favorites</Link>
-      <Container maxWidth="md">
+      <Container maxWidth="lg">
         <Route path="/" component={SearchForm} />
         <Route path="/search" component={SearchList} />
         <Route path="/favorites" component={FavoritesList} />
