20a04091de83303bed0ac5f89833111ba9d0e889
Thomas Rohde <trohde@rocketmail.com>
2021-01-28 10:30:14 -0600

    index template

--- src/index.js
+++ src/index.js
@@ -1,5 +1,52 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import createSagaMiddleware from 'redux-saga';
+import logger from 'redux-logger';
+import {takeEvery, put} from 'redux-saga/effects';
+import axios from 'axios';
+
 import App from './components/App/App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+
+
+
+//reducers
+
+
+
+
+
+
+//generator functions
+
+
+
+
+
+
+
+//saga watcher
+function* watcherSaga() {
+
+}; //end watcherSaga
+
+
+
+
+// middleware and storeInstance
+const sagaMiddleware = createSagaMiddleware();
+
+const storeInstance = createStore(
+    combineReducers({
+
+
+    }),
+    applyMiddleware(sagaMiddleware, logger),
+);
+
+sagaMiddleware.run(watcherSaga);
+
+ReactDOM.render(<Provider store={storeInstance} <App /></Provider>, document.getElementById('root'));
