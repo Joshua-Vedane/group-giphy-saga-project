75b78aba8cd2e726c0bd33036f79ed76e8868201
Kevin Burk <percburk@gmail.com>
2021-01-29 11:10:35 -0600

    added dialog to favoitesItem

--- src/components/FavoritesItem/FavoritesItem.jsx
+++ src/components/FavoritesItem/FavoritesItem.jsx
@@ -11,6 +11,10 @@ import {
   Menu,
   MenuItem,
   Typography,
+  Dialog,
+  DialogContent,
+  DialogTitle,
+  TextField,
 } from '@material-ui/core';
 import { DeleteOutline } from '@material-ui/icons';
 
@@ -18,6 +22,13 @@ function FavoritesItem({ entry }) {
   const dispatch = useDispatch();
   const [category, setCategory] = useState('');
   const [anchorEl, setAnchorEl] = useState(null);
+  const [dialogOpen, setDialogOpen] = useState(false);
+  const [newCategory, setNewCategory] = useState('');
+
+  const handleDialogOpen = () => {
+    setDialogOpen(true);
+    setAnchorEl(null);
+  };
 
   const addCategoryToGif = (clickedText) => {
     setCategory(clickedText);
@@ -34,45 +45,67 @@ function FavoritesItem({ entry }) {
   };
 
   return (
-    <Card elevation={4}>
-      <CardActionArea>
-        <Box paddingTop={2} paddingLeft={2} paddingRight={2}>
-          <img src={entry.image_url} />
+    <>
+      <Card elevation={4}>
+        <CardActionArea>
+          <Box paddingTop={2} paddingLeft={2} paddingRight={2}>
+            <img src={entry.image_url} />
+          </Box>
+        </CardActionArea>
+        <CardContent>
+          <Box minHeight={25}>
+            <Typography variant="body1" align="center">
+              {category}
+            </Typography>
+          </Box>
+        </CardContent>
+        <Box display="flex" alignItems="center" justifyContent="center">
+          <CardActions>
+            <Button color="primary" onClick={handleOpenMenu}>
+              Categories
+            </Button>
+            <Menu
+              anchorEl={anchorEl}
+              keepMounted
+              open={Boolean(anchorEl)}
+              onClose={() => setAnchorEl(null)}
+            >
+              <MenuItem onClick={() => addCategoryToGif('')}>
+                <em>none</em>
+              </MenuItem>
+              <MenuItem onClick={() => addCategoryToGif('funny')}>
+                Funny
+              </MenuItem>
+              <MenuItem onClick={() => addCategoryToGif('odd')}>Odd</MenuItem>
+              <MenuItem onClick={() => addCategoryToGif('cartoon')}>
+                Cartoon
+              </MenuItem>
+              <MenuItem onClick={() => addCategoryToGif('spicy')}>
+                Spicy
+              </MenuItem>
+              <MenuItem onClick={() => addCategoryToGif('meme')}>Meme</MenuItem>
+              <MenuItem onClick={handleDialogOpen}>Add New Category</MenuItem>
+            </Menu>
+            <IconButton onClick={handleDelete}>
+              <DeleteOutline />
+            </IconButton>
+          </CardActions>
         </Box>
-      </CardActionArea>
-      <CardContent>
-        <Box minHeight={25}>
-          <Typography variant="body1" align="center">
-            {category}
-          </Typography>
-        </Box>
-      </CardContent>
-      <Box display="flex" alignItems="center" justifyContent="center">
-        <CardActions>
-          <Button color="primary" onClick={handleOpenMenu}>
-            Categories
-          </Button>
-          <Menu
-            anchorEl={anchorEl}
-            keepMounted
-            open={Boolean(anchorEl)}
-            onClose={() => setAnchorEl(null)}
-          >
-            <MenuItem onClick={() => addCategoryToGif('')}>
-              <em>none</em>
-            </MenuItem>
-            <MenuItem onClick={() => addCategoryToGif('funny')}>Funny</MenuItem>
-            <MenuItem onClick={() => addCategoryToGif('odd')}>Odd</MenuItem>
-            <MenuItem onClick={() => addCategoryToGif('cartoon')}>Cartoon</MenuItem>
-            <MenuItem onClick={() => addCategoryToGif('spicy')}>Spicy</MenuItem>
-            <MenuItem onClick={() => addCategoryToGif('meme')}>Meme</MenuItem>
-          </Menu>
-          <IconButton onClick={handleDelete}>
-            <DeleteOutline />
-          </IconButton>
-        </CardActions>
-      </Box>
-    </Card>
+      </Card>
+      <Dialog open={dialogOpen} onClose={addNewCategory}>
+        <DialogTitle>Add New Category</DialogTitle>
+        <DialogContent>
+          <TextField
+            value={newCategory}
+            onChange={(event) => setNewCategory(event.target.value)}
+          />
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={addNewCategory}>Add</Button>
+          <Button onClick={() => setDialogOpen(false)}>Cancel</Button>
+        </DialogActions>
+      </Dialog>
+    </>
   );
 }
 
